d57cd0e581ec699a9bd82647d517fb2548b6d540
otv0604 <smallassassin@hotmail.co.jp>
2023-03-17 23:47:34 +0900

    rev1.1 材料が途中までになるバグの修正

--- kabutansc.py
+++ kabutansc.py
@@ -12,18 +12,20 @@ def Str2Int(num):
     return float(n)
 
 
-# 注目ニュース一覧ページ
+# [ページ]　注目ニュース一覧
 url = "https://kabutan.jp/news/marketnews/?category=9"
 html = requests.get(url)
 soup = BeautifulSoup(html.content, "html.parser")
 
-# 今朝の注目ニュースを抽出
+# aタグのテキストを抽出
 for element in soup.find_all("a"):
-    # テキストに「今朝」が含まれているか調べる
+    # 「今朝」が含まれている記事のhref属性値をlinkに代入
     if "今朝" in element.text:
         link = element.get("href")
+        break
 
-# 今朝の注目ニュースページ
+
+# [ページ]　当日の注目ニュース
 url = "https://kabutan.jp" + link
 req = requests.get(url)
 html = req.text
@@ -33,62 +35,72 @@ dom = lxml.html.fromstring(html)
 target_xpath = '//*[@id="shijyounews"]/article/div'
 scraped_data = dom.xpath(target_xpath)
 
-# 銘柄名、URL、材料を取得
+# 銘柄名、銘柄URL、材料を取得
 meigaralists, codelists, descriptions = [], [], []
 for news in scraped_data:
     articles = tostring(news, encoding="utf-8").decode().replace("\n", "")
-    # 7行目から3step毎の各情報を取得する
-    for i, codelist in enumerate(articles.split("<br/>")[7:]):
-        # 現状悪材料以下は取得しない設定となっている
-        if "【悪材料】" in codelist:
+    # 空白行を削除
+    articles = list(filter(lambda x: x != "", articles.split("<br/>")))
+    # 【好材料】等の行を削除
+    articles = list(filter(lambda x: "【" not in x, articles))
+    # 4行目から2step毎の各情報を取得する
+    for i, codelist in enumerate(articles[3:]):
+        # サーバー負荷軽減
+        time.sleep(1)
+        # 1行ずつ表示確認
+        print(i, codelist)
+        # ループ終了判定
+        if "※★は上昇" in codelist:
             break
-        elif i % 3 == 0:
-            # 銘柄名一覧
+        # 偶数列
+        elif i % 2 == 0:
+            # 銘柄名リストに追加
             meigaralists.append(codelist.split()[0])
-            # コードURL一覧
+            # コードURLリストに追加
             codelists.append("https://kabutan.jp" + codelist.split('"')[1])
-        elif i % 3 == 1:
-            # 材料一覧
+        # 奇数列
+        elif i % 2 == 1:
+            # 材料リストに追加
             descriptions.append(codelist.split()[0])
 
-# 個別銘柄ページ
+
+# [ページ]　銘柄
 for i in range(len(codelists)):
+    # サーバー負荷軽減
+    time.sleep(1)
     url = codelists[i]
     req = requests.get(url)
-    time.sleep(0.2)
     soup = BeautifulSoup(req.text, "html.parser")
     # 銘柄名抽出
     meigara = soup.select("#kobetsu_left dd")
     # 株価抽出
     price = soup.select("#kobetsu_left tr")
     try:
-        yesterdayowarine = Str2Int(meigara[0].getText()[:-8])
+        # 前日終値の日付部分を削除し成形　他株価は成形のみ
+        yesterday_endprice = Str2Int(meigara[0].getText()[:-8])
         openprice = Str2Int(price[0].select("td")[0].getText())
         highprice = Str2Int(price[1].select("td")[0].getText())
         lowprice = Str2Int(price[2].select("td")[0].getText())
         endprice = Str2Int(price[3].select("td")[0].getText())
-        # 始値から5%以上あがった銘柄のみを抽出
-        if ((highprice - yesterdayowarine) / yesterdayowarine) - (
-            (openprice - yesterdayowarine) / yesterdayowarine
-        ) > 0.05:
-            # excelに保存
-            wb = ol.load_workbook("D:\Down/kabutan.xlsx")
-            # zshから実行する場合のファイルパス
-            # wb = ol.load_workbook("/mnt/d/Down/kabutan.xlsx")
+        # 始値から5%以上上昇orストップ高張り付き銘柄のみ抽出
+        if ((highprice - openprice) / yesterday_endprice) > 0.05 or (
+            highprice - lowprice == 0 and highprice - yesterday_endprice > 0
+        ):
+            # excelに保存する処理
+            wb = ol.load_workbook("kabutan.xlsx")
             ws = wb["Sheet1"]
             max_row = ws.max_row + 1
+            # 書き込んだ行を出力
             print(max_row)
             ws.cell(row=max_row, column=1).value = datetime.datetime.now()
             ws.cell(row=max_row, column=2).value = str(meigaralists[i])
-            ws.cell(row=max_row, column=3).value = int(yesterdayowarine)
+            ws.cell(row=max_row, column=3).value = int(yesterday_endprice)
             ws.cell(row=max_row, column=4).value = int(openprice)
             ws.cell(row=max_row, column=5).value = int(highprice)
             ws.cell(row=max_row, column=6).value = int(lowprice)
             ws.cell(row=max_row, column=7).value = int(endprice)
             ws.cell(row=max_row, column=8).value = str(descriptions[i])
-            wb.save("D:\Down/kabutan.xlsx")
-            # zshから実行する場合のファイルパス
-            # wb.save("/mnt/d/Down/kabutan.xlsx")
+            wb.save("kabutan.xlsx")
     # 例外処理
     except Exception as e:
         print(e.message)
